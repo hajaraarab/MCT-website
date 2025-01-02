e48912f6e7183253cf19af411b2ccf32e2fd1436
hajaraarab <hajar.aarab@student.kdg.be>
2025-01-02 16:42:24 +0100

    "About" en "Latest Posts" secties toegevoegd aan de homepagina

--- site/templates/home.php
+++ site/templates/home.php
@@ -4,4 +4,22 @@
     <img src="/public/images/kdg-school.png" class="school-image">
 </div>
 
-<?php snippet('footer') ?>
+<div class="about-latest-container">
+    <div class="about">
+
+        <!-- ->html vermeid XXS aanvallen, en zorgt ervoor dat de tekst correct wordt weergegeven in html -->
+        <p class="primary-heading"><?= $page->about_title()->html() ?></p>
+
+        <p class="paragraph-text"><?= nl2br($page->about_text()->html()) ?></p>
+    </div>
+
+    <div class="latest-posts">
+        
+        <p class="primary-heading"><?= $page->last_post_title()->html() ?></p>
+        <p class="paragraph-text"><?= nl2br($page->last_post_text()->html()) ?></p>
+
+        <a href="https://www.instagram.com/mct.kdg/">
+            <button class="primary-btn">Follow us</button>
+        </a>
+    </div>
+</div>
