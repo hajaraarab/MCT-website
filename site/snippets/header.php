8e83752324e6b7dba01da44aa3ab5c53fc9eb38e
hajaraarab <hajar.aarab@student.kdg.be>
2025-01-07 01:03:16 +0100

    Header mobielvriendelijk gemaakt

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -10,35 +10,77 @@
 </head>
 <body>
 
-    <div class="wrapper">
-        <header class="header">
+<div class="overlay"></div>
 
-            <div class="logo">
-                <img src="/public/images/kdg-logo.png" class="logo-img">
-                <a href="<?= $site->url() ?>" class="logo-title"><?= $site->title() ?></a>
-            </div>
+<div class="wrapper">
+    <header class="header">
+        <div class="logo">
+            <img src="/public/images/kdg-logo.png" class="logo-img">
+            <a href="<?= $site->url() ?>" class="logo-title"><?= $site->title() ?></a>
+        </div>
 
+    <button class="menu-btn" aria-label="Toggle menu">
+        <div class="hamburger-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5" />
+            </svg>
+        </div>
 
-            <div class="header-content">
-                <nav class="menu">
-                    <ul>
-                        <?php foreach($site->children()->listed() as $item): ?>
-                            <li><a href="<?= $item->url() ?>" class="<?= r($item->isActive(), 'active') ?>"><?= $item->title() ?></a></li>
-                        <?php endforeach ?>
-                    </ul>
-                </nav>  
+        <div class="close-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>
+        </div>
+    </button>
 
+    <div class="header-content">
+        <nav class="menu-desktop">
+            <ul>
+                <?php foreach($site->children()->listed() as $item): ?>
+                    <li><a href="<?= $item->url() ?>" class="<?= r($item->isActive(), 'active') ?>"><?= $item->title() ?></a></li>
+                <?php endforeach ?>
+            </ul>
+        </nav>  
 
-                <form action="" method="get" class="search">
-                    <input type="text" name="search" placeholder="Search in site" value="">
-                    
-                    <button type="submit" class="search-btn">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                        </svg>
-                    </button>
-                </form>
-            </div>
-
-        </header>
+        <form action="" method="get" class="search-desktop">
+            <input type="text" name="search" placeholder="Search in site" value="">
+            
+            <button type="submit" class="search-btn">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+            </button>
+        </form>
     </div>
+
+
+    </header>
+</div>
+
+    <div class="menu-phone">
+        <ul class="menu-items">
+            <?php foreach($site->children()->listed() as $item): ?>
+                <li><a href="<?= $item->url() ?>" class="<?= r($item->isActive(), 'active') ?>"><?= $item->title() ?></a></li>
+            <?php endforeach ?>
+        </ul>
+    </div>
+
+
+
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        const menuButton = document.querySelector('.menu-btn');
+        const menu = document.querySelector('.menu-phone');
+        const overlay = document.querySelector('.overlay'); 
+        
+        menuButton.addEventListener('click', () => {
+            if(menu.style.display === 'none' || menu.style.display === '') {
+                menu.style.display = "block"; 
+                overlay.style.display = "block";  
+            } else {
+                menu.style.display = "none"; 
+                overlay.style.display = "none"; 
+            }
+        });
+    });
+</script>
